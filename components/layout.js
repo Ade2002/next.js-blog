76666732ff47085b64b35671f8242b5eb5bf53c2
Ade2002 <sikirusulaimon2002@gmail.com>
2022-08-28 18:35:37 +0100

    blog post layouts

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,11 @@
 import styles from "./styles/layout.module.scss";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+const name = "Your Name";
+export const siteTitle = "Next.js Sample Website";
 const Layout = ({ children }) => {
   return <div className={styles.container}>{children}</div>;
 };
